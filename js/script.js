c1401899708e80fa08e92fa3cd495185de48118f
LafondTheo <theo03100@live.fr>
2023-05-25 22:57:10 +0200

    Navbar responsive

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 $(window).on("load", function () {
-  var thatPD;
+  var navbar;
   class PageDefaultSetting {
       constructor() {
-          thatPD = this;
-          thatPD.init();
+          navbar = this;
+          navbar.init();
       }
 
       /* Cette méthode initialise différentes fonctionnalités de la page, telles qu'un carrousel et des gestionnaires d'événements, et effectue des modifications sur certains éléments de formulaire.
       Son rôle est de préparer la page pour l'interaction utilisateur. */
       init() {
-          thatPD.windowScroll();
-          $(window).scroll(thatPD.windowScroll);
-          $(".center-btn-area li").on("click", thatPD.scrollToSection);
-          $(".sticky-menu").on("click", thatPD.openNav);
+          navbar.windowScroll();
+          $(window).scroll(navbar.windowScroll);
+          $(".navbar-centre li").on("click", navbar.scrollToSection);
+          $(".menu-lateral").on("click", navbar.openNav);
       }
 
       /* Cette méthode réagit au défilement de la page par l'utilisateur, ajustant ainsi l'apparence de la barre de navigation en fonction de la position de défilement.
       Elle contrôle le comportement de la barre de navigation en fonction de l'action de défilement. */
       windowScroll() {
           if ($(window).scrollTop() > 100 && window.innerWidth > 768) {
-              $(".top-nav-sec").addClass("fixed-nav");
-              if ($(".top-nav-wrapper").hasClass("clicked")) {
-                  thatPD.openNav.call($(".sticky-menu"));
+              $(".navbar-sec").addClass("fixed-nav");
+              if ($(".navbar-container").hasClass("clicked")) {
+                  navbar.openNav.call($(".menu-lateral"));
               }
           } else if ($(window).scrollTop() == 0) {
-              $(".top-nav-sec").removeClass("fixed-nav");
+              $(".navbar-sec").removeClass("fixed-nav");
           }
       }
 
@@ -65,15 +65,15 @@ $(window).on("load", function () {
           $("body, html").stop().animate({
               scrollTop: goTo
           }, 1500);
-          if ($(".top-nav-wrapper").hasClass("opened")) {
-              $(".side-menu-burger i").trigger("click");
+          if ($(".navbar-container").hasClass("opened")) {
+              $(".menu-lateral-mobile-burger i").trigger("click");
           }
       }
 
       /* Ouvre la Navbar lors d'un click sur l'icone du menu burger
       Son rôle est de fournir une fonctionnalité de barre de navigation ouvrable à l'utilisateur. */
       openNav() {
-          if ($(".top-nav-sec.fixed-nav")) {
+          if ($(".navbar-sec.fixed-nav")) {
               $(this).parent().parent().parent().toggleClass("clicked");
               if ($(this).parent().parent().parent().hasClass("clicked")) {
                   $(this).children("i").removeClass("fa-bars").addClass("fa-times");
@@ -91,17 +91,17 @@ $(window).on("load", function () {
     }
 
     init() {
-        $(".side-menu-burger i").on("click", this.sideMenuClicked);
+        $(".menu-lateral-mobile-burger i").on("click", this.sideMenuClicked);
     }
 
     sideMenuClicked() {
-        $(".top-nav-wrapper").toggleClass("opened");
-        if ($(".top-nav-wrapper").hasClass("opened")) {
+        $(".navbar-container").toggleClass("opened");
+        if ($(".navbar-container").hasClass("opened")) {
             $(this).removeClass("fa-bars").addClass("fa-times");
-            $(".top-nav-wrapper .top-nav .mobile-bottom-contact").css("transition", ".1s .4s");
+            $(".navbar-container .navbar .mobile-bottom-contact").css("transition", ".1s .4s");
         } else {
             $(this).removeClass("fa-times").addClass("fa-bars");
-            $(".top-nav-wrapper .top-nav .mobile-bottom-contact").css("transition", ".1s");
+            $(".navbar-container .navbar .mobile-bottom-contact").css("transition", ".1s");
         }
     }
 }
